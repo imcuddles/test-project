2ef4152128dd695005e10672fa51b6d8a107ee66
imcuddles <imcuddles@gmail.com>
2014-12-03 14:39:59 +0900

    Update helloWorld.java

--- helloWorld.java
+++ helloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main (String[] args) {
-    System.out.println("foo bar Hello World !!!!");
+    System.out.println("hoge fuga Hello World !!!!");
   }
 } 
